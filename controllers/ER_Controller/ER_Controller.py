82cdb0a4b697f84edd75a9eca688cacfcdb06e8f
ra4ing <3055039434@qq.com>
2023-11-11 10:55:31 +0000

    路口右转 47号，40秒

--- controllers/ER_Controller/ER_Controller.py
+++ controllers/ER_Controller/ER_Controller.py
@@ -255,7 +255,7 @@ class Controller:
         print("GA optimization terminated.\n")
 
     def run_best(self):
-        for i in range(46, 47):
+        for i in range(47, 48):
             print("++++++++++++++++++++++++++++++++++++++++++++++")
             print("Best {}".format(i))
             self.trainer.genotype = np.load("../module/Best{}.npy".format(i))
